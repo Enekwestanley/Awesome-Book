856b7c0fc4868ed15ad7a26389d105d6807639a8
AlejandraKeber <alejandrakeber@gmail.com>
2022-09-20 15:19:10 -0500

    Fix js and css lintern errors

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-//Book class: Represents a book
+/* eslint-disable max-classes-per-file */
+// Book class: Represents a book
 class Book {
   constructor(title, author, id) {
-      this.title = title;
-      this.author = author;
-      this.id = id;
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
+
+// store class: Handles storage
+class store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = store.getBooks();
+
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(id) {
+    const books = store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.id === id) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
 // UI class: Handles UI Task
 class UI {
   static displayBooks() {
-      
-      const books = store.getBooks();
-      books.forEach ((book) => UI.addBookToList(book))
+    const books = store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
   }
-  static addBookToList(book) {
-      const list = document.querySelector('.main')
-      const section = document.createElement('section')
-      section.classList.add('list-holder')
 
-      section.innerHTML = `<ul class="book-section">
+  static addBookToList(book) {
+    const list = document.querySelector('.main');
+    const section = document.createElement('section');
+    section.classList.add('list-holder');
+
+    section.innerHTML = `<ul class="book-section">
       <div class="book-caption">
           <li class="title">"${book.title}"</li> 
           <li class="by">by</li>
@@ -32,83 +64,49 @@ class UI {
           </li>
       </div>
   </ul>
-  `
-  list.appendChild(section)
-}
+  `;
+    list.appendChild(section);
+  }
 
-static deleteBook(el) {
-    if(el.classList.contains('del')) {
-        el.parentElement.parentElement.parentElement.parentElement.remove();
-    (el.parentElement.parentElement.parentElement)
+  static deleteBook(el) {
+    if (el.classList.contains('del')) {
+      el.parentElement.parentElement.parentElement.parentElement.remove();
     }
-}
+  }
 
-static clearFields() {
-     document.querySelector('#Title').value = '';  
-     document.querySelector('#Author').value = ''; 
-} 
-}
-
-// store class: Handles storage
-class store {
-static getBooks() {
-    let books;
-    if(localStorage.getItem('books') === null) {
-        books = [];
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    return books;
-}
-
-static addBook(book) {
-   const books = store.getBooks();
-
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
-}
-
-static removeBook(id) {
-    const books = store.getBooks();
-
-    books.forEach((book, index) => {
-        if(book.id === id) {
-            books.splice(index, 1)
-        }
-    });
-    localStorage.setItem('books', JSON.stringify(books))
-}
+  static clearFields() {
+    document.querySelector('#Title').value = '';
+    document.querySelector('#Author').value = '';
+  }
 }
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-// Get values from form
-    const title = document.querySelector('#Title').value;
-    const author = document.querySelector('#Author').value;
-// Generate random number per seconds
-    const id = Date.now().toString();
+  e.preventDefault();
+  // Get values from form
+  const title = document.querySelector('#Title').value;
+  const author = document.querySelector('#Author').value;
+  // Generate random number per seconds
+  const id = Date.now().toString();
 
-// Instantiate book  
-    const book = new Book(title, author,id);
-    
-    
-// Add book to UI
-    UI.addBookToList(book);
-// Add book to store
-    store.addBook(book);
-// clear fields
-    UI.clearFields();
-})
+  // Instantiate book
+  const book = new Book(title, author, id);
+
+  // Add book to UI
+  UI.addBookToList(book);
+  // Add book to store
+  store.addBook(book);
+  // clear fields
+  UI.clearFields();
+});
 
 // Event: Remove a book
 document.querySelector('.main').addEventListener('click', (e) => {
-const id = document.querySelector('.id')  
-// Remove book from UI  
-    UI.deleteBook(e.target)
-// Remove from store 
-store.removeBook(id.textContent)
-})
+  const id = document.querySelector('.id');
+  // Remove book from UI
+  UI.deleteBook(e.target);
+  // Remove from store
+  store.removeBook(id.textContent);
+});
